59045f330476974a4e03c6da1e82111b09004348
Maxime Vidori <maxime.vidori@gmail.com>
2017-07-01 10:32:04 +0200

    Change architecture of project

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,7 @@ var app = new Vue({
   el: '#app',
   created: function() {
     var that = this;
-    fetch.get("js/books.json", {
+    fetch.get("assets/json/books.json", {
       success: function(resp) {
         that.books.push.apply(that.books, resp.data.books)
       }
